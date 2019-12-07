44c452821e8c438a49b5da72760c1285e503b73e
冯毅 <fengyi@fengyideMacBook-Pro.local>
2019-12-07 12:18:26 +0800

    init

--- information/src/main/java/com/huayy99/portal/information/InformationApplication.java
+++ information/src/main/java/com/huayy99/portal/information/InformationApplication.java
@@ -1,13 +1,16 @@
 package com.huayy99.portal.information;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class InformationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InformationApplication.class, args);
+		System.out.println("Hua Yi Yang Information App Start ----");
 	}
 
 }
